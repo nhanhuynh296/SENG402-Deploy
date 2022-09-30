09ee9691e6f64928300357a875afa92960514cfa
nhanhuynh296 <62057640+nhanhuynh296@users.noreply.github.com>
2022-10-01 11:27:17 +1300

    Add files via upload
    Cloned from UC Gitlab

---
+++ client/src/views/SENG406 Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <h1>Update your pc for FREE</h1>
+    <p>First, make your terminal to be admin by sudo su</p>
+    <p>Then, execute this command:</p>
+    <p id="test">sudo apt update && sudo apt upgrade</p>
+    <v-btn onclick="this.test()">click</v-btn>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "SENG406 Demo",
+  data() {
+  },
+  methods: {
+    test() {
+    }
+  },
+  created() {
+    this.test()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
